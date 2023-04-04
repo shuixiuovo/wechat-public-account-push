d400f10f3b4d2d3625b454c136eea874c639032c
shuixiuovo <129828016+shuixiuovo@users.noreply.github.com>
2023-04-04 15:15:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olFCA50L6AlEWakD7q2SXAeX0k8Y',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Zds-twUI4JyFi4etNxh46lDQa8l5vN3ff_TT_c9aLgY',
+      useTemplateId: 'BzND1pzE1m5NxMB6hEYHaiaV_7TDAil1eDo52wmvBls',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
