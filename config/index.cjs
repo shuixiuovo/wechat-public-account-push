199458d467ace0e30b40bf17e4a681a56230e82a
shuixiuovo <129828016+shuixiuovo@users.noreply.github.com>
2023-04-04 14:20:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,22 +8,22 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx21a2d9aaecde89ba',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'e8e1b4059656e4de0ec6cf7c06d15d64',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '河南',
+  CITY: '鹤壁',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '心如',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'olFCA50L6AlEWakD7q2SXAeX0k8Y',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'vNyLYjCXWcFb6medjy40YKMI0zG46TioDisvkERjLSw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
