edd581da2add3e492864cf726ca34776e8d164a4
shuixiuovo <129828016+shuixiuovo@users.noreply.github.com>
2023-04-04 16:17:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,7 +6,7 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-USE_PASSAGE: 'push-deer',
+USE_PASSAGE:'push-deer',
   // 使用微信测试号：公众号APP_ID
 //   APP_ID: 'wx21a2d9aaecde89ba',
 
@@ -21,7 +21,7 @@ USE_PASSAGE: 'push-deer',
       // 想要发送的人的名字
       name: '心如宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU21505To3CwKGB61knLgyTlRTYOYyS7ewdGUdzu',
+      id: 'PDU21505TJfzoCjLbSdjWknMH7swyr34x2MLw3zeo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
