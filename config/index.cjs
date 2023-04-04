7af0afd658fbff452f49f57c3b74899c0bffddaa
shuixiuovo <129828016+shuixiuovo@users.noreply.github.com>
2023-04-04 15:13:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '心如',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'olFCA54W8lwZWy53rmA-KtGIENuA',
+      id: 'olFCA50L6AlEWakD7q2SXAeX0k8Y',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'kLEQmc3gvoL3_BGAFAB3jxE_1a9Fz2mxaHnkZ1_mWJw',
+      useTemplateId: 'Zds-twUI4JyFi4etNxh46lDQa8l5vN3ff_TT_c9aLgY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
