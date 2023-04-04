31e87e4cb5c74d5f8f1c98f7a3df2bef82b97e0a
shuixiuovo <129828016+shuixiuovo@users.noreply.github.com>
2023-04-04 16:12:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,12 +6,12 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx21a2d9aaecde89ba',
+//   APP_ID: 'wx21a2d9aaecde89ba',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: 'e8e1b4059656e4de0ec6cf7c06d15d64',
+//   APP_SECRET: 'e8e1b4059656e4de0ec6cf7c06d15d64',
 
   PROVINCE: '河南',
   CITY: '鹤壁',
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '心如',
+      name: '心如宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'olFCA50L6AlEWakD7q2SXAeX0k8Y',
+      id: 'PDU21505To3CwKGB61knLgyTlRTYOYyS7ewdGUdzu',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'txVf6zYfVIjsbGhsMhtdSaU3nJ3d4Pe1mwKot5ULkPk',
+      useTemplateId: '0001
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
