41cd21daefb9727545c1a0ded2ee64d4a48a5cab
shuixiuovo <129828016+shuixiuovo@users.noreply.github.com>
2023-04-06 18:08:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,7 +20,7 @@ const USER_CONFIG = {
     {
    
       // 想要发送的人的名字
-      name: '心如宝贝',
+      name: 'name',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU21505TJfzoCjLbSdjWknMH7swyr34x2MLw3zeo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
